4fd13d94d14d76a23b3b3fd8604f9dc075b5bc2c
Isa Gestoso <107648353+devisag@users.noreply.github.com>
2023-10-06 10:09:14 -0300

    Update slack.py

--- slack.py
+++ slack.py
@@ -6,7 +6,7 @@ import os #provides ways to access the Operating System and allows us to read th
 load_dotenv()
 my_wb = os.getenv("SECRET_WEBHOOK")
 
-web_hook_url = f'https://hooks.slack.com/services/{my_wb}'
+web_hook_url = 'https://hooks.slack.com/services/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'
 
 slack_mg = {
    'text': 'El grafico de google trends fue actualizado con éxito!'
